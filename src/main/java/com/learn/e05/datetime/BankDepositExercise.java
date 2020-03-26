83fd26f7a55641d4feab6918c9d5385258aff7ce
ubuntu <ubuntu@ip-172-31-95-151.ec2.internal>
2020-03-26 13:22:03 +0000

    Added few methods in DATE exercise

--- src/main/java/com/learn/e05/datetime/BankDepositExercise.java
+++ src/main/java/com/learn/e05/datetime/BankDepositExercise.java
@@ -1,5 +1,41 @@
 package com.learn.e05.datetime;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class BankDepositExercise {
+	
+	final static Logger log = LoggerFactory.getLogger(BankDepositExercise.class);
+	
+	public static void main(String[] args) {
+		log.debug("MaturityDate : {}",getMaturityDate("12/03/2010",Period.of(2, 2, 2)));
+		log.debug("InvestmentPeriod : {}",getInvestmentPeriod("12/03/2010","12/08/2012"));
+	}
+	
+	/**
+	 * Method should take investment date and duration as input and return the maturity date as a string in the format <dd-mmm-yyyy>
+	 * 
+	 */
+	public static String getMaturityDate(String investmentDate, Period duration) {
+		LocalDate invDate=LocalDate.parse(investmentDate,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		LocalDate matDate=invDate.plusYears(duration.getYears()).plusMonths(duration.getMonths()).plusDays(duration.getDays());
+		return matDate.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+	}
+	
+	/**
+	 * Method should take investment date and maturity dates as input and return the duration as a string in the format <x years, y months>
+	 * 
+	 */
+	public static String getInvestmentPeriod(String investmentDate, String maturityDate) {
+		DateTimeFormatter format=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		LocalDate invDate=LocalDate.parse(investmentDate,format);
+		LocalDate matDate=LocalDate.parse(maturityDate,format);
+		Period p=Period.between(invDate, matDate);
+		return p.getYears()+" years, "+p.getMonths()+" months";
+	}
 
 }
--- src/main/java/com/learn/e05/datetime/DateTimeExercise.java
+++ src/main/java/com/learn/e05/datetime/DateTimeExercise.java
@@ -1,5 +1,60 @@
 package com.learn.e05.datetime;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.learn.e02.streams.Player;
+
+
 public class DateTimeExercise {
+
+	final static Logger log = LoggerFactory.getLogger(DateTimeExercise.class);
+
+	public static List<Tablet> tabletList = Arrays.asList(new Tablet[] {
+			new Tablet("Ciprofloxacin","Apotex Corp",LocalDate.of(2020, 03, 28),LocalDate.of(2021, 03, 02)),
+			new Tablet("Ciprofloxacin-500mg","Apotex Corp",LocalDate.of(2020, 03, 28),LocalDate.of(2020, 05, 02)),
+			new Tablet("Ciprofloxacin-250mg","Apotex Corp",LocalDate.of(2020, 03, 28),LocalDate.of(2020, 07, 02))
+	});
+	
+	public static void main(String[] args) {
+		log.debug("Expiring Tables in 5 months: {}",getExpiringTables(5));
+		log.debug("List of Tablets, in the ascending order of expiry date:");
+		log.debug(getExpiringTabletsSorted().toString());
+	}
+	
+	/**
+	 * method should take number of months as parameter and return a List of tablet names expiring within the given months from today
+	 */
+	public static List<String> getExpiringTables(int months){
+		return tabletList.stream()
+				.filter(t -> testDate(t.getExpiryDate(),months))
+				.map(Tablet::getTabletName)
+				.collect(Collectors.toList());
+	}
+	
+	private static boolean testDate(LocalDate input,int months) {
+		LocalDate today = LocalDate.now().plusMonths(months);
+		boolean output = input.isBefore(today);
+		return output;
+	}
+	
+	/**
+	 * return a  List of Tablets, in the ascending order of expiry date
+	 */
+	public static List<String> getExpiringTabletsSorted(){
+		return tabletList.stream()
+				.sorted(Comparator.comparing(Tablet::getExpiryDate))
+		.map(Tablet::getTabletName)
+		.collect(Collectors.toList());
+		 
+	}
 
 }
